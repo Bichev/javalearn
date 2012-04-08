d57767c48d7edf9f336eafbd1782d1327dfe5804
Vladimir Bichev <vl.bichev@gmail.com>
2012-04-08 23:18:15 +0400

    learn

---
+++ src/main/java/oracletutorial/ArithmeticDemo.java
@@ -0,0 +1,11 @@
+package oracletutorial;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vladimir
+ * Date: 08.04.12
+ * Time: 23:05
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArithmeticDemo {
+}
---
+++ src/main/java/oracletutorial/ArrayDemo.java
@@ -0,0 +1,11 @@
+package oracletutorial;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: vladimir
+ * Date: 08.04.12
+ * Time: 22:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class ArrayDemo {
+}
